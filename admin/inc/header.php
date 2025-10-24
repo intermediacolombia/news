106c7b98c5d5db30713d853a69aa5c8a3c447765
Intermedia Colombia <intermediacolombia@gmail.com>
2025-10-24 13:23:46 -0500

    Implemntacion Acciones masivas

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -2,7 +2,7 @@
 
 <!-- Favicon -->
 <link rel="shortcut icon" type="image/x-icon" href="<?php echo URLBASE; ?><?php echo FAVICON ?>">
-
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <!-- Bootstrap 5 -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
