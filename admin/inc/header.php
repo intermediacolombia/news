f65faf8416a332a61e82480c1f8ea5d4896f68d2
Intermedia Colombia <intermediacolombia@gmail.com>
2025-10-28 16:36:47 -0500

    Nuevas Adiciones

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -35,9 +35,3 @@
 <!-- Script custom -->
 <script src="<?= htmlspecialchars($url) ?>/template/assets/js/departamentos.js" crossorigin="anonymous"></script>
 
-<style>
-        :root {
-            --primary: <?= $sys['primary'] ?? '#007bff' ?>;
-            --color-hover-link: <?= $sys['color-hover-link'] ?? '#0056b3' ?>;
-        }
-    </style>
