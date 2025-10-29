1f1cec658c77a099fe45fe13fddc765de86f5461
Intermedia Colombia <intermediacolombia@gmail.com>
2025-10-29 10:58:00 -0500

    Correccion 404

--- template/newsers/404.php
+++ template/newsers/404.php
@@ -1,20 +1,26 @@
-<!-- 404 Start -->
-        <div class="container-fluid py-5">
-            <div class="container py-5 text-center">
-                <ol class="breadcrumb justify-content-center mb-5">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item"><a href="#">Pages</a></li>
-                    <li class="breadcrumb-item active text-dark">404</li>
-                </ol>
-                <div class="row justify-content-center">
-                    <div class="col-lg-6">
-                        <i class="bi bi-exclamation-triangle display-1 text-secondary"></i>
-                        <h1 class="display-1">404</h1>
-                        <h1 class="mb-4">Page Not Found</h1>
-                        <p class="mb-4">We’re sorry, the page you have looked for does not exist in our website! Maybe go to our home page or try to use a search?</p>
-                        <a class="btn link-hover border border-primary rounded-pill py-3 px-5" href="index.html">Go Back To Home</a>
-                    </div>
-                </div>
+<!-- 404 Inicio -->
+<div class="container-fluid py-5">
+    <div class="container py-5 text-center">
+        <ol class="breadcrumb justify-content-center mb-5">
+            <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+            <li class="breadcrumb-item"><a href="#">Páginas</a></li>
+            <li class="breadcrumb-item active text-dark">404</li>
+        </ol>
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <i class="bi bi-exclamation-triangle display-1 text-secondary"></i>
+                <h1 class="display-1">404</h1>
+                <h1 class="mb-4">Página No Encontrada</h1>
+                <p class="mb-4">
+                    Lo sentimos, la página que estás buscando no existe en nuestro sitio web.
+                    Puedes regresar a la página de inicio o intentar usar el buscador.
+                </p>
+                <a class="btn link-hover border border-primary rounded-pill py-3 px-5" href="/">
+                    Volver al Inicio
+                </a>
             </div>
         </div>
-        <!-- 404 End -->
+    </div>
+</div>
+<!-- 404 Fin -->
+
