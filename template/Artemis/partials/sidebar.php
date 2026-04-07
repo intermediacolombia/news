b26be743cd3d9ba06484a4757de82ba0548c44bf
Intermedia Colombia <intermediacolombia@gmail.com>
2026-04-06 19:59:22 -0500

    Fix require path in Artemis sidebar

--- template/Artemis/partials/sidebar.php
+++ template/Artemis/partials/sidebar.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../../inc/config.php';
-require_once __DIR__ . '/../../inc/translations.php';
+require_once __DIR__ . '/../../../inc/config.php';
+require_once __DIR__ . '/../../../inc/translations.php';
 
 if (!function_exists('img_url')) {
     function img_url(?string $path): string {
